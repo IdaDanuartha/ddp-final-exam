d43ad0a8fd0b899ffe954d97cdc14ee877220c27
IdaDanuartha <danuart21@gmail.com>
2024-01-04 00:54:20 +0800

    program make diamond pattern  done

---
+++ 10-DiamondPattern/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Program to make Diamond Pattern 
+int main()
+{
+  for (int i = 1; i <= 10; ++i) {
+    for (int j = 1; j <= 10 - i; ++j) {
+      cout << " ";
+    }
+    for (int k = 1; k <= 2 * i - 1; ++k) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+
+  for (int i = 10 - 1; i >= 1; --i) {
+    for (int j = 1; j <= 10 - i; ++j) {
+      cout << " ";
+    }
+    for (int k = 1; k <= 2 * i - 1; ++k) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
