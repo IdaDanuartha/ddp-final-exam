9d12caec28cc08ee344bb5a20bcb5ef2b1ca81b0
IdaDanuartha <danuart21@gmail.com>
2024-01-19 21:51:01 +0800

    tes

--- 10-DiamondPattern/main.cpp
+++ 10-DiamondPattern/main.cpp
@@ -7,7 +7,7 @@ int main()
   for (int i = 1; i <= 10; ++i) {
     for (int j = 1; j <= 10 - i; ++j) {
       cout << " ";
-    }
+    } 
     for (int k = 1; k <= 2 * i - 1; ++k) {
       cout << "*";
     }
